fd7819554c7102a1c5a28d98d7b12bbcfaa9d09a
Josphat Mwangi <josphatwanjiruw@gmail.com>
2023-03-29 11:28:44 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./config/database');
+require('dotenv').config({
+    path: './.env'
+});
+
+
+
+const main = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(express.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
+
+    app.get('/', (req, res)=>{
+        res.send("Hello Its up and running")
+    });
+
+   
+    app.use('*', (req, res) => res.status(404).send('404 Not Found'));
+    
+    app.listen(process.env.PORT || 4000, async () => {
+        console.log(`Server Running ${process.env.PORT}`), await db.connect();
+    });
+};
+
+main();
